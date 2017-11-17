84b0d25451e77cb60ea804fbb7134aa3f61314d5
Evgeny <evgenykholodov@gmail.ru>
2017-11-17 09:52:27 +0300

    Add  api's routes login and protect JWT sign

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,58 @@
 var express = require('express');
 const router = express.Router();
-
+var jwt =require('jsonwebtoken');
 
 router.get('/' , function (req,res) {
 
     res.json({
-        text: 'api'
+        description: 'My API. Please authenticate!',
+
     });
 
 });
+router.post('/login' , function (req,res) {
+    const user = {id: 3};
+    const token = jwt.sign({user} , 'key');
+    res.json({
+        token: token
 
+    });
+
+
+});
+
+router.get('/protected',ensureToken,function (req,res) {
+    jwt.verify(req.token,'key',function (err,data) {
+        if(err) {
+
+            res.send('something wrong');
+        }
+        else {
+            res.send('your are welcome!!!')
+
+        }
+
+
+    });
+
+
+});
+
+function ensureToken(req,res,next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        const ourToken = bearerHeader.split(" ");
+        const bearerToken = ourToken[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.send("SomethingWrong");
+    }
+
+
+}
 
 
 
 module.exports = router;
+
